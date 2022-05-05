90e6d0ef6837a073e82dba7db0e3706b1eb6842f
ewp10022 <rpofuk@gmail.com>
2022-05-05 08:48:52 +0200

    Initial config

--- init.el
+++ init.el
@@ -1,15 +1,42 @@
-;; Set up package.el to work with MELPA
+;eq; Set up package.el to work with MELPA
 (require 'package)
+
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 (package-refresh-contents)
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
+(defvar my-packages
+  '(evil
+    spacemacs-theme
+    paredit
+    cider))
 
-;; Enable Evil
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Enable evil mode
 (require 'evil)
 (evil-mode 1)
 
+(load-theme 'spacemacs-dark t)
+
+;; Reuse error window so it does not popup on random location
+(add-to-list 'same-window-buffer-names "*cider-error*")
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+ '(package-selected-packages '(spacemacs-theme evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
