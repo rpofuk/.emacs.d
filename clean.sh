979fa867cc38150210bda92a34a7319102360216
ewp10022 <robert.pofuk@rbinternational.com>
2022-11-09 16:06:31 +0100

    Fix clean script

--- clean.sh
+++ clean.sh
@@ -4,6 +4,6 @@ rm -rf .dap-breakpoints
 rm -rf .lsp-session-v1
 rm -rf .cache
 
-bash -c 'rm -rfv !("init.el"|"custom"|"clean.sh"|"rpofuk.el")'
+ls -a | grep -vE 'init.el$|custom$|clean.sh$|rpofuk.el$|environment.el$|install.sh$|\.$|^.git.*' | xargs -I {} rm -rf  {}
 
 
