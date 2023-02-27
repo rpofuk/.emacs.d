7e0a67dde284c4ff5242f4941177dfde1be32d5b
ewp10022 <robert.pofuk@rbinternational.com>
2023-02-27 08:07:38 +0100

    Fix install

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(csv-mode evil-collection paredit company flycheck lsp-treemacs which-key lsp-ui cider lsp-mode clojure-mode tabspaces counsel swiper ivy treemacs-tab-bar treemacs-magit treemacs-evil treemacs winum))
+   '(csv-mode evil-collection paredit company flycheck lsp-treemacs lsp-ui cider lsp-mode clojure-mode which-key tabspaces counsel swiper ivy treemacs-tab-bar treemacs-magit treemacs-evil treemacs winum))
  '(safe-local-variable-values
    '((eval let
 	   ((personal-env "~/.emacs.d/environment.el"))
--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ debconf-set-selections <<< "postfix postfix/mailname string $HOSTNAME"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 
 
-apt-get install -y build-essential libgccjit-11-dev
+apt-get install -y build-essential libgccjit-11-dev libwebkit2gtk-4.0-dev
 
 apt-get build-dep -y emacs 
 
@@ -33,6 +33,9 @@ apt-get build-dep -y emacs
 	--with-gpm=no \
 	--with-modules \
 	--with-pgtk \
+	--with-xwidgets \
+	--with-xml2 \
+	--with-gsettings \
 	--with-native-compilation \
 	--with-json \
 	  CFLAGS="-O4 -mtune=native -march=native -fomit-frame-pointer"
